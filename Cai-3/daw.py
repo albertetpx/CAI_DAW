49b3e6449a548fcd0b24171677427a968c1a95c9
Kai <valentina.montana@estudiant.fjaverianas.com>
2022-12-15 15:39:11 +0100

    Update daw.py

--- Cai-3/daw.py
+++ Cai-3/daw.py
@@ -3,7 +3,7 @@ from flask import Flask, redirect, render_template, request, url_for
 
 db=mysql.connector.connect(host='localhost',
                        user='root',
-                       password='el.moounirejh1',
+                       password='',
                        database="formulario_cai"
                        )
 
