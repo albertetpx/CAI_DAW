70fa38c51cbf33b06bd2448bf468dbb0feee9e5f
Ramon Guabiraba <104869271+Rahguabiraba@users.noreply.github.com>
2023-02-08 23:54:36 +0100

    Actualización de listado de pacientes

--- CAI-DAW/static/funciones.js
+++ CAI-DAW/static/funciones.js
@@ -21,3 +21,48 @@ function abrirMenu() {
     linea3.style.backgroundColor = "white";
   }
 }
+
+let index = function inicio() {
+  location.replace("/");
+}
+
+let registro = function registrar() {
+  location.replace("/registrar");
+}
+
+let lista = function listaPaciente() {
+  location.replace("/lista_paciente");
+}
+
+
+// Get the modal
+var modal = document.getElementById("paciente-details");
+
+//activar listener de los cardView
+var card = document.querySelectorAll(".card");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+//Para cada card qué ha sido pulsado, utilizamos la función
+card.forEach(function (item) {
+	item.onclick = displayModal;
+})
+
+// When the user clicks on the button, open the modal
+function displayModal() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
