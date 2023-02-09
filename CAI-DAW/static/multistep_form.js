fb1f30ebef6ff51995a59c2c685fd4f82d0369f7
Kar_Yan <102352076+Karlenjak@users.noreply.github.com>
2023-02-09 16:29:45 +0100

    Cambios de  Registro y de js

--- CAI-DAW/static/multistep_form.js
+++ CAI-DAW/static/multistep_form.js
@@ -1,8 +1,8 @@
 
-// function ocultarBotonRegistrar(){
-//   let butonFinal = document.getElementById("butonFinal");
-//   butonFinal.style.display = "block";
-// }
+function ocultarBotonRegistrar(){
+  let butonFinal = document.getElementById("butonFinal");
+  butonFinal.style.display = "block";
+}
 
 function pasarForm1() {
   let form1 = document.getElementById("part1");
@@ -17,7 +17,19 @@ function pasarForm1() {
   let buton2 = document.getElementById("button2");
   buton2.style.display = "block";
 
+  // Atras 
+  let buton1_atras = document.getElementById("button1_atras");
+  buton1_atras.style.display = "block";
 
+  let buton2_atras = document.getElementById("button2_atras");
+  buton2_atras.style.display = "none";
+
+
+  let form2 = document.getElementById("part2");
+  form2.style.display = "none";
+
+  let form2_1 = document.getElementById("part2_1");
+  form2_1.style.display = "none";
 }
 
 function pasarForm1_1() {
@@ -32,7 +44,24 @@ function pasarForm1_1() {
 
   let buton3 = document.getElementById("button3");
   buton3.style.display = "block";
+  
+  // atras 
 
+  let buton2_atras = document.getElementById("button2_atras");
+  buton2_atras.style.display = "block";
+
+  let buton3_atras = document.getElementById("button3_atras");
+  buton3_atras.style.display = "none";
+
+  let form2_1 = document.getElementById("part2_1");
+  form2_1.style.display = "none";
+
+  let form3 = document.getElementById("part3");
+  form3.style.display = "none";
+
+  // ocult anterior 
+  let form1 = document.getElementById("part1");
+  form1.style.display = "none";
   
 }
 
@@ -48,8 +77,23 @@ function pasarForm2() {
 
   let buton4 = document.getElementById("button4");
   buton4.style.display = "block";
+  // atras
 
+  let buton3_atras = document.getElementById("button3_atras");
+  buton3_atras.style.display = "block";
+
+  let buton4_atras = document.getElementById("button4_atras");
+  buton4_atras.style.display = "none";
+
+  let form3 = document.getElementById("part3");
+  form3.style.display = "none";
+
+  let form3_1 = document.getElementById("part3_1");
+  form3_1.style.display = "none";
   
+  // ocilt anterior 
+  let form1_1 = document.getElementById("part1_1");
+  form1_1.style.display = "none";
 }
 
 function pasarForm2_1() {
@@ -64,8 +108,24 @@ function pasarForm2_1() {
 
   let buton5 = document.getElementById("button5");
   buton5.style.display = "block";
+  atras
 
-  
+  // atras 
+  let buton4_atras = document.getElementById("button4_atras");
+  buton4_atras.style.display = "block";
+
+  let buton5_atras = document.getElementById("button5_atras");
+  buton5_atras.style.display = "none";
+
+  let form3_1 = document.getElementById("part3_1");
+  form3_1.style.display = "none";
+
+  let form4 = document.getElementById("part4");
+  form4.style.display = "none";
+
+  // anterior 
+  let form2 = document.getElementById("part2");
+  form2.style.display = "none";
 }
 
 function pasarForm3() {
@@ -81,7 +141,22 @@ function pasarForm3() {
   let buton6 = document.getElementById("button6");
   buton6.style.display = "block";
 
-  
+  let buton5_atras = document.getElementById("button5_atras");
+  buton5_atras.style.display = "block";
+
+  let buton6_atras = document.getElementById("button6_atras");
+  buton6_atras.style.display = "none";
+
+  let form4 = document.getElementById("part4");
+  form4.style.display = "none";
+
+  let form5 = document.getElementById("part5");
+  form5.style.display = "none";
+
+  // anterior 
+  let form2_1 = document.getElementById("part2_1");
+  form2_1.style.display = "none";
+
 }
 
 function pasarForm3_1() {
@@ -96,8 +171,24 @@ function pasarForm3_1() {
 
   let buton7 = document.getElementById("button7");
   buton7.style.display = "block";
+  //  atras 
 
+  let buton6_atras = document.getElementById("button6_atras");
+  buton6_atras.style.display = "block";
+
+  let buton7_atras = document.getElementById("button7_atras");
+  buton7_atras.style.display = "none";
+
+  let form4_1 = document.getElementById("part4_1");
+  form4_1.style.display = "none";
   
+  let form5 = document.getElementById("part5");
+  form5.style.display = "none";
+
+  let form3 = document.getElementById("part3");
+  form3.style.display = "none";
+
+
 }
 
 function pasarForm4() {
@@ -113,7 +204,22 @@ function pasarForm4() {
   let buton7_1 = document.getElementById("button7_1");
   buton7_1.style.display = "block";
 
-  
+  let buton7_atras = document.getElementById("button7_atras");
+  buton7_atras.style.display = "block";
+
+  let buton7_1_atras = document.getElementById("button8_atras");
+  buton7_1_atras.style.display = "none";
+
+  // atras 
+  let form5 = document.getElementById("part5");
+  form5.style.display = "none";
+
+  let form6 = document.getElementById("part6");
+  form6.style.display = "none";
+
+  let form3_1 = document.getElementById("part3_1");
+  form3_1.style.display = "none";
+
 }
 
 function pasarForm4_1() {
@@ -129,7 +235,22 @@ function pasarForm4_1() {
   let buton8 = document.getElementById("button8");
   buton8.style.display = "block";
 
-  
+  // atras 
+
+  let buton7_1__atras = document.getElementById("button7_1_atras");
+  buton7_1__atras.style.display = "block";
+
+  let buton8_atras = document.getElementById("button8_atras");
+  buton8_atras.style.display = "none";
+
+  // atras 
+  let form6 = document.getElementById("part6");
+  form6.style.display = "none";
+  let form7 = document.getElementById("part7");
+  form7.style.display = "none";
+  // ocult anterior 
+  let form4 = document.getElementById("part4");
+  form4.style.display = "none";
 }
 
 function pasarForm5() {
@@ -145,7 +266,22 @@ function pasarForm5() {
   let buton9 = document.getElementById("button9");
   buton9.style.display = "block";
 
-  
+  // atras
+  let buton8_atras = document.getElementById("button8_atras");
+  buton8_atras.style.display = "block";
+
+  let buton9_atras = document.getElementById("button9_atras");
+  buton9_atras.style.display = "none";
+
+
+  let form7 = document.getElementById("part7");
+  form7.style.display = "none";
+
+  let form8 = document.getElementById("part8");
+  form8.style.display = "none";
+
+  let form4_1 = document.getElementById("part4_1");
+  form4_1.style.display = "none";
 }
 
 function pasarForm6() {
@@ -160,8 +296,22 @@ function pasarForm6() {
 
   let buton9 = document.getElementById("button10");
   buton9.style.display = "block";
+  // atras
+  let buton9_atras = document.getElementById("button9_atras");
+  buton9_atras.style.display = "block";
 
-  
+  let buton10_atras = document.getElementById("button10_atras");
+  buton10_atras.style.display = "none";
+
+  let form8 = document.getElementById("part8");
+  form8.style.display = "none";
+
+  let form9 = document.getElementById("part9");
+  form9.style.display = "none";
+
+  let form5 = document.getElementById("part5");
+  form5.style.display = "none";
+
 }
 
 function pasarForm7() {
@@ -177,7 +327,20 @@ function pasarForm7() {
   let buton10 = document.getElementById("button11");
   buton10.style.display = "block";
 
+    // atras
+    let buton10_atras = document.getElementById("button10_atras");
+    buton10_atras.style.display = "block";
   
+    let buton11_atras = document.getElementById("button11_atras");
+    buton11_atras.style.display = "none";
+
+    let form9 = document.getElementById("part9");
+    form9.style.display = "none";
+    let form10 = document.getElementById("part10");
+    form10.style.display = "none";
+
+    let form6 = document.getElementById("part6");
+    form6.style.display = "none";
 }
 
 function pasarForm8() {
@@ -193,7 +356,21 @@ function pasarForm8() {
   let buton11 = document.getElementById("button12");
   buton11.style.display = "block";
 
-  
+      // atras
+      let buton11_atras = document.getElementById("button11_atras");
+      buton11_atras.style.display = "block";
+    
+      let buton12_atras = document.getElementById("button12_atras");
+      buton12_atras.style.display = "none";
+
+      let form10 = document.getElementById("part10");
+      form10.style.display = "none";
+
+      let form11 = document.getElementById("part11");
+      form11.style.display = "none";
+      
+      let form7 = document.getElementById("part7");
+      form7.style.display = "none";
 }
 
 function pasarForm9() {
@@ -208,8 +385,23 @@ function pasarForm9() {
 
   let buton12 = document.getElementById("button13");
   buton12.style.display = "block";
-
   
+
+     // atras
+     let buton12_atras = document.getElementById("button12_atras");
+     buton12_atras.style.display = "block";
+   
+     let buton13_atras = document.getElementById("button13_atras");
+     buton13_atras.style.display = "none";
+
+     let form11 = document.getElementById("part11");
+     form11.style.display = "none";
+
+     let form12 = document.getElementById("part12");
+     form12.style.display = "none";
+
+     let form8 = document.getElementById("part8");
+     form8.style.display = "none";
 }
 
 function pasarForm10() {
@@ -225,6 +417,22 @@ function pasarForm10() {
   let buton14 = document.getElementById("button14");
   buton14.style.display = "block";
   
+
+     // atras
+     let buton13_atras = document.getElementById("button13_atras");
+     buton13_atras.style.display = "block";
+   
+     let buton14_atras = document.getElementById("button14_atras");
+     buton14_atras.style.display = "none";
+
+     
+    let form12 = document.getElementById("part12");
+    form12.style.display = "none";
+    let form13 = document.getElementById("part13");
+    form13.style.display = "none";
+
+     let form9 = document.getElementById("part9");
+     form9.style.display = "none";
 }
 
 function pasarForm11() {
@@ -240,6 +448,21 @@ function pasarForm11() {
   let buton15 = document.getElementById("button15");
   buton15.style.display = "flex";
   
+     // atras
+     let buton14_atras = document.getElementById("button14_atras");
+     buton14_atras.style.display = "block";
+   
+     let buton15_atras = document.getElementById("button15_atras");
+     buton15_atras.style.display = "none";
+
+     let form13 = document.getElementById("part13");
+     form13.style.display = "none";
+
+     let form14 = document.getElementById("part14");
+     form14.style.display = "none";
+
+     let form10 = document.getElementById("part10");
+     form10.style.display = "none";
 }
 
 function pasarForm12() {
@@ -254,6 +477,22 @@ function pasarForm12() {
 
   let buton16 = document.getElementById("button16");
   buton16.style.display = "flex";
+
+     // atras
+     let buton15_atras = document.getElementById("button15_atras");
+     buton15_atras.style.display = "block";
+   
+     let buton16_atras = document.getElementById("button16_atras");
+     buton16_atras.style.display = "none";
+
+     let form14 = document.getElementById("part14");
+     form14.style.display = "none";
+
+     let form15 = document.getElementById("part15");
+     form15.style.display = "none";
+
+     let form11 = document.getElementById("part11");
+     form11.style.display = "none";
 }
 
 function pasarForm13() {
@@ -268,6 +507,19 @@ function pasarForm13() {
 
   let buton17 = document.getElementById("button17");
   buton17.style.display = "flex";
+
+     // atras
+     let buton16_atras = document.getElementById("button16_atras");
+     buton16_atras.style.display = "block";
+   
+     let buton17_atras = document.getElementById("button17_atras");
+     buton17_atras.style.display = "none";
+
+     let form15 = document.getElementById("part15");
+     form15.style.display = "none";
+
+     let form12 = document.getElementById("part12");
+     form12.style.display = "none";
 }
 
 function pasarForm14() {
@@ -280,9 +532,308 @@ function pasarForm14() {
   let buton17 = document.getElementById("button17");
   buton17.style.display = "none";
 
-  let buton18 = document.getElementById("button18");
-  buton18.style.display = "flex";
+  // let buton18 = document.getElementById("button18");
+  // buton18.style.display = "flex";
 
   let butonFinal = document.getElementById("butonFinal");
     butonFinal.style.display = "block";
+
+       // atras
+       let buton17_atras = document.getElementById("button17_atras");
+       buton17_atras.style.display = "block";
+     
+       let buton18_atras = document.getElementById("button18_atras");
+       buton18_atras.style.display = "none";
+
+       let form13 = document.getElementById("part13");
+       form13.style.display = "none";
 }
+
+
+
+
+
+// Boton atras 
+
+function pasarForm1_atras() {
+
+  let form1 = document.getElementById("part1");
+  form1.style.display = "flex";
+
+  let form1_1 = document.getElementById("part1_1");
+  form1_1.style.display = "none";
+
+  let buton1 = document.getElementById("button1_atras");
+  buton1.style.display = "none";
+
+  let buton2 = document.getElementById("button2_atras");
+  buton2.style.display = "none";
+
+  
+}
+
+function pasarForm1_1_atras() {
+  let form1_1 = document.getElementById("part1_1");
+  form1_1.style.display = "flex";
+
+  let form2 = document.getElementById("part2");
+  form2.style.display = "none";
+
+  let buton2 = document.getElementById("button2_atras");
+  buton2.style.display = "none";
+
+  let buton3 = document.getElementById("button3_atras");
+  buton3.style.display = "block";
+
+  
+}
+
+function pasarForm2_atras() {
+  let form2 = document.getElementById("part2");
+  form2.style.display = "flex";
+
+  let form2_1 = document.getElementById("part2_1");
+  form2_1.style.display = "none";
+
+  let buton3 = document.getElementById("button3_atras");
+  buton3.style.display = "none";
+
+  let buton4 = document.getElementById("button4_atras");
+  buton4.style.display = "block";
+
+  
+}
+
+function pasarForm2_1_atras() {
+  let form2_1 = document.getElementById("part2_1");
+  form2_1.style.display = "none";
+
+  let form3 = document.getElementById("part3");
+  form3.style.display = "flex";
+
+  let buton4 = document.getElementById("button4_atras");
+  buton4.style.display = "none";
+
+  let buton5 = document.getElementById("button5_atras");
+  buton5.style.display = "block";
+
+  
+}
+
+function pasarForm3_atras() {
+  let form3 = document.getElementById("part3");
+  form3.style.display = "none";
+
+  let form3_1 = document.getElementById("part3_1");
+  form3_1.style.display = "flex";
+
+  let buton5 = document.getElementById("button5_atras");
+  buton5.style.display = "none";
+
+  let buton6 = document.getElementById("button6_atras");
+  buton6.style.display = "block";
+
+  
+}
+
+function pasarForm3_1_atras() {
+  let form3_1 = document.getElementById("part3_1");
+  form3_1.style.display = "none";
+
+  let form4 = document.getElementById("part4");
+  form4.style.display = "flex";
+
+  let buton6 = document.getElementById("button6_atras");
+  buton6.style.display = "none";
+
+  let buton7 = document.getElementById("button7_atras");
+  buton7.style.display = "block";
+
+  
+}
+
+function pasarForm4_atras() {
+  let form4 = document.getElementById("part4");
+  form4.style.display = "none";
+
+  let form4_1 = document.getElementById("part4_1");
+  form4_1.style.display = "flex";
+
+  let buton7 = document.getElementById("button7_atras");
+  buton7.style.display = "none";
+
+  let buton7_1 = document.getElementById("button7_1_atras");
+  buton7_1.style.display = "block";
+
+  
+}
+
+function pasarForm4_1_atras() {
+  let form4_1 = document.getElementById("part4_1");
+  form4_1.style.display = "none";
+
+  let form5 = document.getElementById("part5");
+  form5.style.display = "flex";
+
+  let buton7_1 = document.getElementById("button7_1_atras");
+  buton7_1.style.display = "none";
+
+  let buton8 = document.getElementById("button8_atras");
+  buton8.style.display = "block";
+
+  
+}
+
+function pasarForm5_atras() {
+  let form5 = document.getElementById("part5");
+  form5.style.display = "none";
+
+  let form6 = document.getElementById("part6");
+  form6.style.display = "flex";
+
+  let buton8 = document.getElementById("button8_atras");
+  buton8.style.display = "none";
+
+  let buton9 = document.getElementById("button9_atras");
+  buton9.style.display = "block";
+
+  
+}
+
+function pasarForm6_atras() {
+  let form6 = document.getElementById("part6");
+  form6.style.display = "none";
+
+  let form7 = document.getElementById("part7");
+  form7.style.display = "flex";
+
+  let buton8 = document.getElementById("button9_atras");
+  buton8.style.display = "none";
+
+  let buton9 = document.getElementById("button10_atras");
+  buton9.style.display = "block";
+
+  
+}
+
+function pasarForm7_atras() {
+  let form7 = document.getElementById("part7");
+  form7.style.display = "none";
+
+  let form8 = document.getElementById("part8");
+  form8.style.display = "flex";
+
+  let buton9 = document.getElementById("button10_atras");
+  buton9.style.display = "none";
+
+  let buton10 = document.getElementById("button11_atras");
+  buton10.style.display = "block";
+
+  
+}
+
+function pasarForm8_atras() {
+  let form8 = document.getElementById("part8");
+  form8.style.display = "none";
+
+  let form9 = document.getElementById("part9");
+  form9.style.display = "flex";
+
+  let buton10 = document.getElementById("button11_atras");
+  buton10.style.display = "none";
+
+  let buton11 = document.getElementById("button12_atras");
+  buton11.style.display = "block";
+
+  
+}
+
+function pasarForm9_atras() {
+  let form9 = document.getElementById("part9");
+  form9.style.display = "none";
+
+  let form10 = document.getElementById("part10");
+  form10.style.display = "flex";
+
+  let buton11 = document.getElementById("button12_atras");
+  buton11.style.display = "none";
+
+  let buton12 = document.getElementById("button13_atras");
+  buton12.style.display = "block";
+
+  
+}
+
+function pasarForm10_atras() {
+  let form10 = document.getElementById("part10");
+  form10.style.display = "none";
+
+  let form11 = document.getElementById("part11");
+  form11.style.display = "flex";
+
+  let buton13 = document.getElementById("button13_atras");
+  buton13.style.display = "none";
+
+  let buton14 = document.getElementById("button14_atras");
+  buton14.style.display = "block";
+  
+}
+
+function pasarForm11_atras() {
+  let form11 = document.getElementById("part11");
+  form11.style.display = "none";
+
+  let form12 = document.getElementById("part12");
+  form12.style.display = "flex";
+
+  let buton14 = document.getElementById("button14_atras");
+  buton14.style.display = "none";  
+  
+  let buton15 = document.getElementById("button15_atras");
+  buton15.style.display = "flex";
+  
+}
+
+function pasarForm12_atras() {
+  let form12 = document.getElementById("part12");
+  form12.style.display = "none";
+
+  let form13 = document.getElementById("part13");
+  form13.style.display = "flex";
+
+  let buton15 = document.getElementById("button15_atras");
+  buton15.style.display = "none";
+
+  let buton16 = document.getElementById("button16_atras");
+  buton16.style.display = "block";
+}
+
+function pasarForm13_atras() {
+  let form13 = document.getElementById("part13");
+  form13.style.display = "none";
+
+  let form14 = document.getElementById("part14");
+  form14.style.display = "flex";
+
+  let buton16 = document.getElementById("button16_atras");
+  buton16.style.display = "none";
+
+  let buton17 = document.getElementById("button17_atras");
+  buton17.style.display = "block";
+}
+
+function pasarForm14_atras() {
+  let form14 = document.getElementById("part14");
+  form14.style.display = "none";
+
+  let form15 = document.getElementById("part15");
+  form15.style.display = "none";
+ 
+  let buton17 = document.getElementById("button17_atras");
+  buton17.style.display = "block";
+
+  // let buton18 = document.getElementById("button18");
+  // buton18.style.display = "flex";
+
+}
+
